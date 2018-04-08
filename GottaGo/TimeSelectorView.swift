fa7c45369b457e409ea6a618d15d59bf9321b2e3
Dhruv Mathur <dhruv.mathur2@loblaw.ca>
2018-04-08 08:38:15 -0400

    first commit

---
+++ GottaGo/TimeSelectorView.swift
@@ -0,0 +1,10 @@
+//
+//  TimeSelectorView.swift
+//  GottaGo
+//
+//  Created by Dhruv Mathur (LCL) on 4/7/18.
+//  Copyright © 2018 Dhruv Mathur (LCL). All rights reserved.
+//
+
+import Foundation
+
---
+++ GottaGo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  GottaGo
+//
+//  Created by Dhruv Mathur (LCL) on 3/21/18.
+//  Copyright © 2018 Dhruv Mathur (LCL). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func leaveTimePicker(_ sender: UIDatePicker) {
+        
+    }
+    @IBAction func arriveTimePicker(_ sender: UIDatePicker) {
+        
+    }
+    
+    @IBAction func gottaGoButton(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
