7ca16aa960435a873328848d327fb47b32babaf7
Dhruv <dhruvm@me.com>
2018-08-29 15:30:21 -0400

    initial parse complete

--- GottaGo/API.swift
+++ GottaGo/API.swift
@@ -19,7 +19,7 @@ class API{
     //var departure_time = "departure_time=\()"
     static var arrival_time = "arrival_time=\(propertyKey.userDefaults.integer(forKey: "workTimePicker"))"
     
-    func makeURL() {
+    func makeURL(){
         let  originformatted = API.origin.replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
         let  destinationformatted = API.destination.replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
         let parameters = "\(originformatted)&\(destinationformatted)&\(API.mode)&\(API.arrival_time)&\(API.key)"
@@ -27,18 +27,42 @@ class API{
         print(finalURLInString)
         // let requestURL: URL = URL(string: API.baseURL+parameters)!
         
-        Alamofire.request(finalURLInString, method: .get, parameters: ["":""], encoding: URLEncoding.default, headers: nil).responseJSON { (response:DataResponse<Any>) in
+        let finalResponse = Alamofire.request(finalURLInString, method: .get, parameters: ["":""], encoding: URLEncoding.default, headers: nil).responseJSON { response in
             
             switch(response.result) {
             case .success(_):
                 if response.result.value != nil{
-                    print(response.result.value!)
+                    //print(response.result.value!)
                 }
                 break
             case .failure(_):
                 print(response.result.error!)
                 break
             }
+            if let result = response.result.value {
+                let JSON = result as! NSDictionary
+                let ap1 = JSON["routes"] as! NSArray
+                let ap2 = ap1[0] as! NSDictionary
+                let ap3 = ap2["legs"] as! NSArray
+                let apString = ap2["overview_polyline"] as! NSDictionary
+                let ap4 = ap3[0] as! NSDictionary
+                let apArrivalTime = ap4["arrival_time"] as! NSDictionary
+                let apDuration = ap4["duration"] as! NSDictionary
+                let apDepartureTime = ap4["departure_time"] as! NSDictionary
+                let arrivalTime = apArrivalTime["text"] as! String
+                let duration = apDuration["text"] as! String
+                let departureTime = apDepartureTime["text"] as! String
+                let endAddress = ap4["end_address"] as! String
+                let startAddress = ap4["start_address"] as! String
+                let overviewPolyline = apString["points"] as! String
+                print(arrivalTime)
+                print(departureTime)
+                print(duration)
+                print(endAddress)
+                print(startAddress)
+                print(overviewPolyline)
+            }
         }
+        
     }
 }
