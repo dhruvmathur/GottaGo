79a8b299c763a9eaf0a88abfc5c89e081586427a
Dhruv <dhruvm@me.com>
2018-08-29 13:20:30 -0400

    gmsmapview added

--- GottaGo/ResultScreen.swift
+++ GottaGo/ResultScreen.swift
@@ -11,8 +11,9 @@ import GoogleMaps
 
 class ResultScreen: UIViewController{
 
-    @IBOutlet weak var uiMapView: UIView!
-    override func loadView() {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+//    override func loadView() {
 //        // Create a GMSCameraPosition that tells the map to display the
 //        // coordinate -33.86,151.20 at zoom level 6.
 //        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
@@ -20,13 +21,28 @@ class ResultScreen: UIViewController{
 //        let mapView = GMSMapView.map(withFrame: mapFrame, camera: camera)
 //        //self.view.addSubview(mapView)
 //        view.addSubview(mapView)
-// 
+//
 //        // Creates a marker in the center of the map.
 //        let marker = GMSMarker()
 //        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
 //        marker.title = "Sydney"
 //        marker.snippet = "Australia"
 //        marker.map = mapView
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: 48.857165, longitude: 2.354613, zoom: 8.0)
+        mapView.camera = camera
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
 }
